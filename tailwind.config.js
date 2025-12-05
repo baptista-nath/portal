fe756bae42d811ef4dbdaef48f436575123c0280
Nathalia Baptista <na.ti.baptistacosta@gmail.com>
2025-12-05 17:41:02 -0300

    Ajustes noticia procon

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,8 @@ module.exports = {
         'segoe': ['"Segoe UI"', 'Roboto', 'Arial', 'sans-serif'],
         'cambria': ['Cambria', 'serif'],
         'calibri': ['Calibri', 'sans-serif'],
+        // Classe ajustada para 'inter'
+        'inter': ['Inter', 'sans-serif'],
       },
     },
   },
